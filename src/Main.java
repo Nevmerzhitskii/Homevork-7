f0ed82c2fca39f46b12af3fee96d0b156a131bdc
Nevmerzhitskii <comdeo13@yandex.ru>
2023-12-06 23:07:35 +0900

    Homework Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,95 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1 ");
+
+        int [] weight = new int[3];
+        weight [0] = 1;
+        weight [1] = 2;
+        weight [2] = 3;
+
+        float [] symbols = { 1.57f, 7.654f, 9.986f};
+
+        int [] something = {1, 2, 3, 4, 5 };
+
+        System.out.println("Задача №2 ");
+
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(weight[i] + ",");
+        }
+        System.out.println("");
+        for (int i = 0; i < symbols.length; i++) {
+            System.out.print(symbols[i] + ",");
+        }
+        System.out.println("");
+        for (int i = 0; i < something.length; i++) {
+            System.out.print(something[i] + ",");
+        }
+        System.out.println("");
+
+        System.out.println("Задача №3 ");
+
+        for (int i = weight.length - 1; i >= 0; i--) {
+            System.out.print(weight[i] + ", ");
+        }
+        System.out.println( "");
+        for (int i = symbols.length - 1; i >= 0 ; i--) {
+            System.out.print(symbols[i] + ", ");
+        }
+        System.out.println("");
+        for (int i = something.length - 1; i >= 0; i--) {
+            System.out.print(something[i] + ", ");
+        }
+        System.out.println("");
+
+
+        System.out.println("Задача №4 ");
+
+        int [] arr = {1, 2, 3};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr [i] % 2 != 0 ){
+                arr[i] ++;
+            }
+        }System.out.println(Arrays.toString(arr));
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
